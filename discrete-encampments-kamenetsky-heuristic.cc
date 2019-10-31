3fae416879061ed472ddb820cc0bd16b8bc40dd4
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-10-31 13:40:59 -0400

    Give "dek" the ability to read and collate its old output files.
    If "dek-out.txt" exists, then we'll try to read it on startup.
    Whatever results are in that file will be recorded, either as
    the starting point for further random exploration or as static
    data to be parroted back out. This means you can explore a particular
    range (for example, MIN_N=5 MAX_N=10) without blowing away your
    results from any previous run.
    
    If "dek-out.txt" exists but contains unparseable data, we'll just
    assert-fail. If that happens, you can always remove the file
    to make "dek" runnable again.

--- discrete-encampments-kamenetsky-heuristic.cc
+++ discrete-encampments-kamenetsky-heuristic.cc
@@ -38,8 +38,9 @@
 #include <utility>
 #include <vector>
 
-static constexpr int MIN_N = 3;
-static constexpr int MAX_N = 16;
+static constexpr int MIN_N = 14;
+static constexpr int MAX_N = 20;
+static const char FILENAME[] = "dek-out.txt";
 
 enum ScoreType { Extra = 0, Max = 1 };
 struct ScoreType_Extra { static constexpr int value = ScoreType::Extra; };
@@ -47,26 +48,25 @@ struct ScoreType_Max { static constexpr int value = ScoreType::Max; };
 
 class A250000_Base {
 protected:
-    struct CNFG { int c, n, f, g; };
+    struct NCFG { int n, c, f, g; };
 private:
     virtual void do_step() = 0;
+    virtual void do_parseBestString(const std::string& bestString) = 0;
     virtual std::string do_getBestString() const = 0;
-    virtual CNFG do_getCNFG() const = 0;
+    virtual NCFG do_getNCFG() const = 0;
 public:
     virtual ~A250000_Base() = default;
     void step() {
         this->do_step();
     }
+    void parseBestString(const std::string& bestString) {
+        this->do_parseBestString(bestString);
+    }
     std::string getBestString() const {
         return this->do_getBestString();
     }
-    std::pair<int, int> getCN() const { auto cnfg = this->do_getCNFG(); return {cnfg.c, cnfg.n}; }
-    int getFCN() const { auto cnfg = this->do_getCNFG(); return cnfg.f; }
-    int getGCN() const { auto cnfg = this->do_getCNFG(); return cnfg.g; }
-};
-
-struct RCV {
-    int r, c, val;
+    int getF() const { auto ncfg = this->do_getNCFG(); return ncfg.f; }
+    int getG() const { auto ncfg = this->do_getNCFG(); return ncfg.g; }
 };
 
 struct xorshift128p {
@@ -93,6 +93,47 @@ struct xorshift128p {
 private:
     // splitmix64 seeded with "1"
     uint64_t m_state[2] = { 0x5692161D100B05E5uLL, 0x910A2DEC89025CC1uLL };
+};
+
+struct NC {
+    // It is important to the output format that N=4,C=3 sorts less-than N=5,C=2.
+    int n, c;
+    friend bool operator<(NC a, NC b) { return a.n < b.n || (a.n == b.n && a.c < b.c); }
+};
+
+struct RCV {
+    int r, c, val;
+};
+
+class Parrot : public A250000_Base {
+    int n_;
+    int c_;
+    int bestMinQueens_;
+    int bestMaxQueens_;
+    std::string bestString_;
+
+public:
+    explicit Parrot(int n, int c) : n_(n), c_(c) {}
+
+private:
+    void do_step() override {}
+    void do_parseBestString(const std::string& bestString) override {
+        bestString_ = bestString;
+        int n, c, f, g;
+        int rc = std::sscanf(bestString.c_str(), "N=%d C=%d min=%d max=%d all=", &n, &c, &f, &g);
+        assert(rc == 4 || !"malformed solution in input file");
+        assert(n == n_ && c == c_);
+        bestMinQueens_ = f;
+        bestMaxQueens_ = g;
+    }
+
+    std::string do_getBestString() const override {
+        return bestString_;
+    }
+
+    NCFG do_getNCFG() const override {
+        return { n_, c_, bestMinQueens_, bestMaxQueens_ };
+    }
 };
 
 template<int N, int C, bool Interesting = (2 <= C && C < N)>
@@ -140,7 +181,7 @@ private:
     std::string do_getBestString() const override {
         return currentBestString_;
     }
-    CNFG do_getCNFG() const override { return { C, N, currentBestMinQueens_, currentBestMaxQueens_ }; }
+    NCFG do_getNCFG() const override { return { N, C, currentBestMinQueens_, currentBestMaxQueens_ }; }
 
     template<class ST>
     struct StructuredScore {
@@ -303,7 +344,7 @@ private:
                 }
 
                 if (s.score > bestScore_) {
-                    currentBestString_ = prettyPrint(s.score, a);
+                    currentBestString_ = prettyPrint(a);
                     bestScore_ = s.score;
                     currentBestMinQueens_ = s.minQueens();
                     currentBestMaxQueens_ = s.maxQueens();
@@ -318,7 +359,7 @@ private:
                 }
 
                 if (s.score > bestScore_) {
-                    currentBestString_ = prettyPrint(s.score, a);
+                    currentBestString_ = prettyPrint(a);
                     bestScore_ = s.score;
                     currentBestMinQueens_ = s.minQueens();
                     currentBestMaxQueens_ = s.maxQueens();
@@ -378,7 +419,15 @@ private:
     }
 
     static char to_digit(int i) {
+        assert(0 <= i && i <= 35);
         return ".123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[i];
+    }
+
+    static int from_digit(char ch) {
+        const char *alphabet = ".123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const char *p = strchr(alphabet, ch);
+        assert(p != nullptr);
+        return (p - alphabet);
     }
 
     static std::string prettyQueens(const int (&queens)[C+1])
@@ -395,7 +444,7 @@ private:
         return std::move(result).str();
     }
 
-    static std::string prettyPrint(int score, const Board<int>& a)
+    static std::string prettyPrint(const Board<int>& a)
     {
         std::ostringstream result;
 
@@ -416,8 +465,45 @@ private:
             }
             result << '\n';
         }
-        result << '\n';
         return std::move(result).str();
+    }
+
+    static Board<int> prettyUnprint(const std::string& bestString) {
+        Board<int> a;
+        int n, c, f, g;
+        int rc = std::sscanf(bestString.c_str(), "N=%d C=%d min=%d max=%d all=", &n, &c, &f, &g);
+        assert(rc == 4 || !"malformed solution in input file");
+        assert(n == N && c == C);
+        assert(f <= N*N / C);
+        assert(f <= g && g <= N*N - f*(C-1));
+        const char *p = strchr(bestString.c_str(), '\n');
+        assert(p != nullptr);
+        ++p;
+        for (int i=0; i < N; ++i) {
+            for (int k=0; k < N; ++k) {
+                a[i][k] = from_digit(*p++);
+            }
+            assert(*p == '\n');
+            ++p;
+        }
+        assert(*p == '\0');
+        return a;
+    }
+
+    void do_parseBestString(const std::string& bestString) override {
+        Board<int> a = prettyUnprint(bestString);
+        auto s = StructuredScore<ScoreType_Extra>::from_board(a);
+        bestA[0] = a;
+        bestScores_[0] = s.score;
+
+        auto sm = StructuredScore<ScoreType_Max>::from_board(a);
+        bestA[Q] = a;
+        bestScores_[Q] = sm.score;
+        bestScore_ = sm.score;
+
+        currentBestString_ = prettyPrint(a);
+        currentBestMinQueens_ = s.minQueens();
+        currentBestMaxQueens_ = s.maxQueens();
     }
 };
 
@@ -425,7 +511,8 @@ template<int N, int C>
 class A250000<N, C, false> : public A250000_Base {
     void do_step() override { exit(0); }
     std::string do_getBestString() const override { return ""; }
-    CNFG do_getCNFG() const override { return { C, N, 0, 0 }; }
+    NCFG do_getNCFG() const override { return { N, C, 0, 0 }; }
+    void do_parseBestString(const std::string&) override {}
 };
 
 template<int Min, int... Is>
@@ -479,28 +566,84 @@ std::unique_ptr<A250000_Base> make_A250000(int n, int c) {
     return make_A250000_n(make_int_range<MIN_N, MAX_N>(), n, c);
 }
 
-std::string make_triangle(const std::map<std::pair<int, int>, int>& fcn)
+template<class F>
+std::string make_triangle(const std::map<NC, std::shared_ptr<A250000_Base>>& m, const F& getValue)
 {
     std::ostringstream result;
     result << "    k=       1  2  3  4  5  6  ...\n";
     result << "          .\n";
     result << "    n=1   .  1  0\n";
     result << "    n=2   .  4  0  0\n";
-    for (int n = MIN_N; n <= MAX_N; ++n) {
+
+    assert(!m.empty());
+    int max_n_to_print = std::prev(m.end())->first.n;
+    assert(max_n_to_print >= MAX_N);
+
+    for (int n = 3; n <= max_n_to_print; ++n) {
         result << "    n=" << std::setw(2) << std::left << n << "   " << std::setw(3) << std::right << n*n;
         for (int c = 2; c <= n+1; ++c) {
-            auto it = fcn.find({c,n});
-            int value = 0;
-            if (it != fcn.end()) {
-                value = it->second;
-            } else if (n == c) {
-                value = (n == 2 || n == 3) ? 0 : 1;
+            auto it = m.find(NC{n,c});
+            if (it != m.end()) {
+                int value = getValue(*it->second);
+                result << std::setw(3) << std::right << value;
+            } else if (c == n) {
+                int value = (n == 2 || n == 3) ? 0 : 1;
+                result << std::setw(3) << std::right << value;
+            } else if (c > n) {
+                result << std::setw(3) << std::right << 0;
+            } else {
+                result << std::setw(3) << std::right << '?';
             }
-            result << std::setw(3) << std::right << value;
         }
         result << "\n";
     }
     return std::move(result).str();
+}
+
+void write_solutions_to_file(const char *filename, const std::map<NC, std::shared_ptr<A250000_Base>>& m)
+{
+    std::ofstream outfile(filename);
+    outfile << make_triangle(m, [](const A250000_Base& a) { return a.getF(); }) << "\n\n";
+    outfile << make_triangle(m, [](const A250000_Base& a) { return a.getG(); }) << "\n\n";
+    for (auto&& kv : m) {
+        outfile << kv.second->getBestString() << '\n';
+    }
+}
+
+void maybe_read_solutions_from_file(const char *filename, std::map<NC, std::shared_ptr<A250000_Base>>& m)
+{
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        return;
+    }
+    std::string line;
+    bool seen_a_grid = false;
+    while (std::getline(infile, line)) {
+        puts(line.c_str());
+        if (line.compare(0, 2, "N=") == 0) {
+            int n, c, f, g;
+            int rc = std::sscanf(line.c_str(), "N=%d C=%d min=%d max=%d all=", &n, &c, &f, &g);
+            assert(rc == 4 || !"input file contained malformed lines");
+
+            std::string bestString = line + "\n";
+            for (int i=0; i < n; ++i) {
+                std::getline(infile, line);
+                puts(line.c_str());
+                assert(bool(infile) || !"input file ended prematurely");
+                bestString += line;
+                bestString += "\n";
+            }
+            auto it = m.find(NC{n, c});
+            if (it == m.end()) {
+                it = m.emplace(NC{n,c}, std::make_shared<Parrot>(n, c)).first;
+            }
+            it->second->parseBestString(bestString);
+
+            seen_a_grid = true;
+        } else if (seen_a_grid && line != "") {
+            assert(!"input file contained malformed lines after the first grid");
+        }
+    }
 }
 
 template<class Duration>
@@ -509,35 +652,34 @@ size_t in_ms(Duration d)
     return static_cast<size_t>(std::chrono::duration_cast<std::chrono::milliseconds>(d).count());
 }
 
-int main() {
+int main()
+{
     auto prestart_time = std::chrono::high_resolution_clock::now();
 
-    std::vector<std::unique_ptr<A250000_Base>> all;
+    std::map<NC, std::shared_ptr<A250000_Base>> to_output;
+    std::vector<std::shared_ptr<A250000_Base>> to_update;
+
     for (int n = MIN_N; n <= MAX_N; ++n) {
         for (int c = 2; c < n; ++c) {
-            all.push_back(make_A250000(n, c));
+            std::shared_ptr<A250000_Base> p = make_A250000(n, c);
+            to_output.emplace(NC{n,c}, p);
+            to_update.push_back(p);
         }
     }
+
+    maybe_read_solutions_from_file(FILENAME, to_output);
 
     auto start_time = std::chrono::high_resolution_clock::now();
     printf("done setup in %zu ms\n", in_ms(start_time - prestart_time));
 
-    std::map<std::pair<int, int>, int> fcn;
-    std::map<std::pair<int, int>, int> gcn;
     for (int iteration = 1; true; ++iteration) {
-        std::string output;
-        for (const auto& a : all) {
+        for (auto&& p : to_update) {
             for (int i=0; i < 8; ++i) {
-                a->step();
+                p->step();
             }
-            output += a->getBestString();
-            fcn[a->getCN()] = a->getFCN();
-            gcn[a->getCN()] = a->getGCN();
         }
-        std::ofstream outfile("dek-out.txt");
-        outfile << make_triangle(fcn) << "\n\n";
-        outfile << make_triangle(gcn) << "\n\n";
-        outfile << output;
+
+        write_solutions_to_file(FILENAME, to_output);
 
         auto finish_time = std::chrono::high_resolution_clock::now();
         printf("%d iterations in %zu ms\n", iteration, in_ms(finish_time - start_time));
